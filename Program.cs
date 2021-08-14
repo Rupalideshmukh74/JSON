38c90be64f2eadd3199872e44d81ac3ee9844da1
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-14 20:38:20 +0530

    [Rupali]Add:welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to JSON Inventory Data Management");
+        }
+    }
+}
